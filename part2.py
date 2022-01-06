54f8baf3fcdc50d7bbe9a6a52bfad0f9183e748e
Duany Pena <94643825+penaduany@users.noreply.github.com>
2022-01-06 19:15:17 +0000

    I fixed the code

--- part2.py
+++ part2.py
@@ -8,3 +8,14 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+n = int(input("Enter a positive number: "))
+ 
+for x in range(1, n + 1):
+ print(x**3)
+
+
+b = int(input("Enter the base: "))
+h = int(input("Enter the height: "))
+ 
+for x in range(h):
+ print("*" * b)
